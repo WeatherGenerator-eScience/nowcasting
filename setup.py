bfa32dd9be4b7ee7a675f5dd8022854261a0541c
MatsRobben <matsrobben@gmail.com>
2025-07-14 21:40:01 +0200

    Update setup.py
    Signed-off-by: MatsRobben <matsrobben@gmail.com>

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+setuptools.setup(
+    name="nowcasting",
+    version="0.0.1",
+    author="Mats Robben",
+    url="https://github.com/MatsRobben/nowcasting",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "numba",
+        "pandas",
+        "matplotlib",
+        "torch",
+        "torchvision",
+        "torchmetrics",
+        "einops",
+        "lightning",
+        "zarr",
+        "netcdf4",
+        "xarray",
+        "dask",
+        "fire",
+        "omegaconf",
+        "tqdm",
+        "opencv-python-headless",
+        "pysteps",
+        "scipy",
+        "codecarbon",
+        "scikit-image", 
+    ],
+    python_requires=">=3.11",
+)
